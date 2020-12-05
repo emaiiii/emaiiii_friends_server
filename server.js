0b14f635a38ea9c1a4e37713114186f4d0acdead
emaiiii <airwickmai@gmail.com>
2020-12-05 14:20:17 -0800

    sign in and sign up done

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+
+
 // link to database
 const db = knex({
   client: 'pg',
@@ -24,79 +29,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 /*sign in post request*/
-app.post('/signin', (req, res) => {
-	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-
-			if(isValid){
-				return db.select('*').from('users')
-					.where('email', '=', req.body.email)
-					.then(user => {
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json("Error: unable to get user"))
-			}
-			else{
-				res.status(400).json("Error: wrong credentials");
-			}
-		})
-		.catch(err => res.status(400).json("Error: wrong credentials"))
-})
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
 
 /*register post request*/
-app.post('/register', (req, res) => {
-	const {fName, lName, email, password} = req.body;
-
-	var hash = bcrypt.hashSync(password);
-
-	// use transcation if you want to make multiple operations on databases
-	// if one fails then they all fail
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-				.returning('*')
-				.insert({
-					fname: fName,
-					lname: lName,
-					email: loginEmail[0],
-					joined: new Date()
-			})
-			.then(user => {
-				res.json(user[0]);
-			})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	.catch(err => res.status(400).json('Error: unable to register'));
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 /*profile get request*/
 /*code can be adjusted to implement any functionality
 that requires you to get users*/
-app.get('/profile/:id', (req, res) => {
-	const {id} = req.params;
-
-	db.select('*').from('users').where({id})
-		.then(user => {
-			if(user.length){
-				res.json(user[0]);
-			}
-			else{ 
-				res.status(400).json("Error: user not found");
-			}
-
-		})
-		.catch(err => res.status(400).json("Error: user not found"))
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
 app.listen(3000, () => {
 	console.log("app is running on port 3000");
