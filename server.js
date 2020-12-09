6a1f76bea4ef8f3faf2c044d61a4616e9a56a8fc
emaiiii <airwickmai@gmail.com>
2020-12-08 21:00:30 -0800

    unfollow needs fix to prevent negative following

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const follow = require('./controllers/follow');
+const unfollow = require('./controllers/unfollow');
 
 
 // link to database
@@ -15,7 +17,7 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : 'postgres',
+    password : 'test',
     database : 'friends'
   }
 });
@@ -33,6 +35,12 @@ app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
 
 /*register post request*/
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+/*follow post request*/
+app.post('/follow/:followed_id/:follower_id', (req, res) => {follow.handleFollow(req, res, db)})
+
+/*following post request*/
+app.post('/unfollow/:unfollowed_id/:unfollower_id', (req, res) => {unfollow.handleUnfollow(req, res, db)})
 
 /*profile get request*/
 /*code can be adjusted to implement any functionality
